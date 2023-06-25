9b3d11c32a124efa8d0e5b0e3fde84375ce3b2f9
Diego Rocha <drochaesc@gmail.com>
2023-06-25 16:37:33 -0600

    Validación de inputs vacíos
    Generando alerta en el HTML

---
+++ js/app.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function(){
+   
+    // Seleccionando elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+
+    // Asignar eventos
+    inputEmail.addEventListener('blur', validar);
+    inputAsunto.addEventListener('blur', validar);
+    inputMensaje.addEventListener('blur', validar);
+
+    function validar(e) {
+        if(e.target.value.trim() === ''){
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`)
+        }
+        else {
+
+        }
+    }
+
+    function mostrarAlerta(mensaje) {
+        //Generar alerta en HTML
+        const error = document.createElement('DIV');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+        //Inyectar el error al formulario
+        formulario.appendChild(error)
+    }
+    
+});
