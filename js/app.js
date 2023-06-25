fa547e7a5c19db1d791a164f61f8f2dbcea2341f
Diego Rocha <drochaesc@gmail.com>
2023-06-25 16:55:56 -0600

    Agregando y eliminando alertas de input obligatorio

--- js/app.js
+++ js/app.js
@@ -13,21 +13,34 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function validar(e) {
         if(e.target.value.trim() === ''){
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`)
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement)
+            return; // Detener ejecución del código
         }
-        else {
-
-        }
+        
+        limpiarAlerta(e.target.parentElement);
     }
 
-    function mostrarAlerta(mensaje) {
-        //Generar alerta en HTML
+    function mostrarAlerta(mensaje, referencia) {
+        // Comprobar si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
+
+        // Generar alerta en HTML
         const error = document.createElement('DIV');
         error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
 
-        //Inyectar el error al formulario
-        formulario.appendChild(error)
+        // Inyectar el error al formulario
+        referencia.appendChild(error)
+    }
+
+    function limpiarAlerta(referencia) {
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
     }
     
 });
